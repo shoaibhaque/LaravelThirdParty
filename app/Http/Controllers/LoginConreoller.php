6670a2202ea69148802a6d13dbd718616ba5a1c3
shoaibhaque <showaibulchowdhury@gmail.com>
2021-01-01 11:59:37 +0600

    updating login controller

---
+++ app/Http/Controllers/LoginConreoller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LoginConreoller extends Controller
+{
+    public function index(){
+    	return view('login.index');
+    }
+
+    public function login(Request $req){
+    	if(count(DB::table('users')->where('username',$req->username)->where('password',$req->password)->first())>0){
+    		return redirect()->route('home.index');
+    	}
+    	else{
+    		$req->session()->flash('msg', 'Invalid Username no or Password');
+    		return redirect()->route('login.index');
+    	}
+    }
+}
